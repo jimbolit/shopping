43cb2869d411e59f7ca2a5d3a2bf4e985d102db9
jimbolit <jameslittle@outlook.co.nz>
2021-02-09 20:39:31 +1300

    made the numbers in categories work

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -1,25 +1,26 @@
 class BasketsController < ApplicationController
 
     def index
+            
+      
+      
             @basket = []
             @basket = session[:basket].select{|x| x["order_quantity"]>0}
-           # puts @basket
+           
             basket_ids = @basket.map {|x| x.values[0]}
-            # puts basket_ids
-            @products = Product.where(id: basket_ids).to_a
-           puts @products.inspect
-            @sub_total = 0
 
-        #   @products = Product.where(id: [1,2,3]).to_a
-        #  print @products
-         # @products = Product.where(id: basket_ids).to_a
-          #  @products = Product.all
+            @basket_products = Product.where(id: basket_ids).to_a
+
+            puts @basket_products
+          
             @sub_total = 0
-            @products.each{
+            @basket_products.each{
                |i| @sub_total = @sub_total + i[:price] * @basket.find{
-                |y| y["product_id"] == i[:id]
-                }.to_h["order_quantity"].to_i
-            }
+                   |y| y["product_id"] == i[:id]
+                   }.to_h["order_quantity"].to_i
+            @subtotal ||= 0
+           }
+      
            
          if @basket == []
            render :basket_empty
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -23,24 +23,25 @@ class CategoriesController < ApplicationController
     #  print @products[0][:name]
     # this is just to see how it prints 
     
-      @basket = session[:basket].select{|x| x[:order_quantity]>0}
+      @basket = session[:basket].select{|x| x["order_quantity"]>0}
       basket_ids = @basket.map {|x| x.values[0]}
      
-    rescue NoMethodError
-    # does not fail if the current basket is empty -  
-    # not in original Sinatra code 
-    # (not sure why it wasn't required)
-
       @basket_products = Product.where(id: basket_ids).to_a
+
+      puts @basket_products
     
       @sub_total = 0
       @basket_products.each{
          |i| @sub_total = @sub_total + i[:price] * @basket.find{
-             |y| y[:product_id] == i[:id]
-             }[:order_quantity]
+             |y| y["product_id"] == i[:id]
+             }.to_h["order_quantity"].to_i
       @subtotal ||= 0
      }
-  
+
+    rescue NoMethodError
+      # does not fail if the current basket is empty -  
+      # not in original Sinatra code 
+      # (not sure why it wasn't required)
     end
 
     def increase_quantity
